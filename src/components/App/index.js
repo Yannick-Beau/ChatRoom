f23aa099d8caba3314333e8cf66142f296c382ba
Yannick-Beau <beauyannick@gmail.com>
2021-07-27 19:57:33 +0200

    messages dynamique

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,14 +2,17 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import Messages from 'src/components/Messages';
+import Form from 'src/components/Form';
+import './styles.scss';
 
+// import data
+import messagesData from 'src/data';
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <Messages messagesData={messagesData} />
+    <Form />
   </div>
 );
 
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: 1,
+    author: 'Jake',
+    message: 'Jake est un génie',
+  },
+  {
+    id: 13,
+    author: 'Ray Holt',
+    message: 'Peralta !',
+  },
+  {
+    id: 4,
+    author: 'Charles Boyle',
+    message: 'Salut Jake',
+  },
+];
