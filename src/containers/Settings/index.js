ba7111abf62b3fb1b9d169ad0950ea7e01c69a5b
Yannick-Beau <beauyannick@gmail.com>
2021-07-29 19:31:41 +0200

    ajout de settings + css + champ controlé et caché/masqué

---
+++ src/containers/Settings/index.js
@@ -0,0 +1,33 @@
+import { connect } from 'react-redux';
+
+// on importe le composant de présentation pour lequel on veut créer un assistant
+import Settings from 'src/components/Settings';
+import { changeInputValue, setShowSettings } from '../../actions';
+
+const mapStateToProps = (state) => ({
+  // nom de la prop : comment récupérer l'info correspondante dans le state
+  inputEmailValue: state.inputEmail,
+  inputPasswordValue: state.inputPassword,
+  showSettings: state.showSettings,
+});
+
+// === mapDispatchToProps : dispatch des actions vers le store
+const mapDispatchToProps = (dispatch) => ({
+  setValue: (newValue, typeInput) => {
+    const action = changeInputValue(newValue, typeInput);
+    dispatch(action);
+  },
+  setShowSettings: () => {
+    dispatch(setShowSettings());
+  },
+});
+
+// ==== création de l'assistant
+// connect crée une fonction sur mesure qui permettra de créer l'assistant
+const fonctionCreation = connect(mapStateToProps, mapDispatchToProps);
+// on applique la fonction fournie par connect sur le composant de présentation
+// pour obtenir l'assistant
+const assistant = fonctionCreation(Settings);
+
+// on exporte l'assistant
+export default assistant;
