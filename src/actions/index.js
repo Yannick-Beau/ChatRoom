21a9060cee718f542883670d53299285ed077cc1
Yannick-Beau <beauyannick@gmail.com>
2021-07-29 13:24:24 +0200

    add composant settings + css

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,21 @@
-export const SET_INPUT_VALUE = 'SET_INPUT_VALUE';
+export const SET_INPUT_MESSAGE_VALUE = 'SET_INPUT_VALUE';
 export const ADD_MESSAGE = 'ADD_MESSAGE';
 
-export const changeInputValue = (value) => ({
-  type: SET_INPUT_VALUE,
-  value: value,
-});
+export const changeInputValue = (value, typeInput) => {
+  switch (typeInput) {
+    case 'message':
+      return {
+        type: SET_INPUT_MESSAGE_VALUE,
+        value: value,
+      };
+
+    default:
+      return {
+        type: SET_INPUT_MESSAGE_VALUE,
+        value: value,
+      };
+  }
+};
 
 export const addMessage = () => ({
   type: ADD_MESSAGE,
